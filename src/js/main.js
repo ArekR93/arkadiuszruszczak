51daae2f9b03ea271335ba3e5b4bdbbab967418e
Arkadiusz <arkadiuszruszczak0@gmail.com>
2024-06-11 15:58:38 +0200

    new files

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,44 @@
-const menuBtn = document.querySelector('.navbar-hamburger-btn')
-const menuList = document.querySelector('.menu-list')
-const timesBtn = document.querySelector('.fa-xmark')
-const hamburgerInner = document.querySelector('.hamburger-inner')
-const menuElements = document.querySelectorAll('.menu-element')
+document.addEventListener('DOMContentLoaded', function(){
 
-const showMenu = () => {
-    if (timesBtn.classList.contains('active-xmark')){
-        timesBtn.classList.remove('active-xmark')
-        hamburgerInner.style.display = 'block'
-        menuList.classList.remove('menu-animation')
-        
+    const menuBtn = document.querySelector('.navbar-hamburger-btn')
+    const menuList = document.querySelector('.menu-list')
+    const timesBtn = document.querySelector('.fa-xmark')
+    const hamburgerInner = document.querySelector('.hamburger-inner')
+    const menuElements = document.querySelectorAll('.menu-element')
+    const cardItem = document.querySelectorAll('.card-item')
 
-    }else{
-        timesBtn.classList.add('active-xmark')
-        hamburgerInner.style.display = 'none'
-        menuList.classList.add('menu-animation')
-        
+    const showMenu = () => {
+        if (timesBtn.classList.contains('active-xmark')){
+            timesBtn.classList.remove('active-xmark')
+            hamburgerInner.style.display = 'block'
+            menuList.classList.remove('menu-animation')
+            
+
+        }else{
+            timesBtn.classList.add('active-xmark')
+            hamburgerInner.style.display = 'none'
+            menuList.classList.add('menu-animation')
+            
+        }
     }
-}
 
+    const checkScroll = () => {
+        console.log(window.scrollY);
+        if(window.scrollY > 500){
+            cardItem.forEach((card) => {
+                card.classList.add('show-cards')
+            })
+        }else{
+            cardItem.forEach((card) => {
+                card.classList.remove('show-cards')
+            })
+        }
+    }
 
-menuElements.forEach((element) => {
-    element.addEventListener('click', showMenu)
+    menuElements.forEach((element) => {
+        element.addEventListener('click', showMenu)
+    })
+
+    window.addEventListener('scroll', checkScroll)
+    menuBtn.addEventListener('click', showMenu)
 })
-
-
-menuBtn.addEventListener('click', showMenu)
-
